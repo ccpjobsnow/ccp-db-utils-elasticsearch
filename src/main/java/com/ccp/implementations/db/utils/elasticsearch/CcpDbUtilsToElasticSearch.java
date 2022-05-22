2669f677991ab7eda89ae8ec1f32eb6b0ea48906
onias <onias@noxxonsat.com.br>
2022-05-22 16:35:05 -0300

    .

--- src/main/java/com/ccp/implementations/db/utils/elasticsearch/CcpDbUtilsToElasticSearch.java
+++ src/main/java/com/ccp/implementations/db/utils/elasticsearch/CcpDbUtilsToElasticSearch.java
@@ -8,6 +8,7 @@ import com.ccp.dependency.injection.CcpImplementation;
 import com.ccp.especifications.db.utils.CcpDbUtils;
 import com.ccp.especifications.http.CcpHttp;
 import com.ccp.especifications.http.CcpHttpHandler;
+import com.ccp.especifications.http.CcpHttpResponseTransform;
 
 @CcpImplementation
 public class CcpDbUtilsToElasticSearch implements CcpDbUtils {
@@ -24,40 +25,40 @@ public class CcpDbUtilsToElasticSearch implements CcpDbUtils {
 	}
 
 	@Override
-	public CcpMapDecorator executeHttpRequest(String url, String method,  int expectedStatus, String body, CcpMapDecorator headers) {
+	public <V> V executeHttpRequest(String url, String method,  int expectedStatus, String body, CcpMapDecorator headers, CcpHttpResponseTransform<V> transformer) {
 		headers = this.getHeaders().putAll(headers);
 		CcpHttpHandler http = new CcpHttpHandler(expectedStatus, this.ccpHttp);
-		CcpMapDecorator executeHttpRequest = http.executeHttpRequest(url, method, headers, body);
+		V executeHttpRequest = http.executeHttpRequest(url, method, headers, body, transformer);
 
 		return executeHttpRequest;
 	}
 
 	@Override
-	public CcpMapDecorator executeHttpRequest(String complemento, String method, int expectedStatus, CcpMapDecorator body,  String[] resources) {
+	public <V> V executeHttpRequest(String complemento, String method, int expectedStatus, CcpMapDecorator body,  String[] resources, CcpHttpResponseTransform<V> transformer) {
 		String path = this.getHeaders().getAsString("DB_URL") +  Arrays.asList(resources).stream().toString()
 				.replace("[", "").replace("]", "").replace(" ", "") + complemento;
 		CcpMapDecorator headers = this.getHeaders();
 		CcpHttpHandler http = new CcpHttpHandler(expectedStatus, this.ccpHttp);
-		CcpMapDecorator executeHttpRequest = http.executeHttpRequest(path, method, headers, body);
+		V executeHttpRequest = http.executeHttpRequest(path, method, headers, body, transformer);
 		return executeHttpRequest;
 	}
 
 	@Override
-	public CcpMapDecorator executeHttpRequest(String url, String method, CcpMapDecorator flows, CcpMapDecorator body) {
+	public <V> V executeHttpRequest(String url, String method, CcpMapDecorator flows, CcpMapDecorator body, CcpHttpResponseTransform<V> transformer) {
 		CcpMapDecorator headers = this.getHeaders();
 		CcpHttpHandler http = new CcpHttpHandler(flows, this.ccpHttp);
 		String path = headers.getAsString("DB_URL") + url;
-		CcpMapDecorator executeHttpRequest = http.executeHttpRequest(path, method, headers, body);
+		V executeHttpRequest = http.executeHttpRequest(path, method, headers, body, transformer);
 		
 		return executeHttpRequest;
 	}
 
 	@Override
-	public CcpMapDecorator executeHttpRequest(String url, String method, int expectedStatus, CcpMapDecorator body) {
+	public <V> V executeHttpRequest(String url, String method, int expectedStatus, CcpMapDecorator body, CcpHttpResponseTransform<V> transformer) {
 		CcpMapDecorator headers = this.getHeaders();
 		CcpHttpHandler http = new CcpHttpHandler(expectedStatus, this.ccpHttp);
 		String path = headers.getAsString("DB_URL") + url;
-		CcpMapDecorator executeHttpRequest = http.executeHttpRequest(path, method, headers, body);
+		V executeHttpRequest = http.executeHttpRequest(path, method, headers, body, transformer);
 		
 		return executeHttpRequest;
 	}
