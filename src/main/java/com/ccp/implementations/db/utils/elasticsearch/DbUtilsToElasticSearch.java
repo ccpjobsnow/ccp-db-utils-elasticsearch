e8b786f9bc04fd45c8aee1cd427388b9a002dd23
onias <onias@noxxonsat.com.br>
2023-07-26 00:22:31 -0300

    .

--- src/main/java/com/ccp/implementations/db/utils/elasticsearch/DbUtilsToElasticSearch.java
+++ src/main/java/com/ccp/implementations/db/utils/elasticsearch/DbUtilsToElasticSearch.java
@@ -1,10 +1,9 @@
 package com.ccp.implementations.db.utils.elasticsearch;
 
-import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Properties;
 
 import com.ccp.decorators.CcpMapDecorator;
+import com.ccp.decorators.CcpStringDecorator;
 import com.ccp.dependency.injection.CcpDependencyInject;
 import com.ccp.especifications.db.utils.CcpDbUtils;
 import com.ccp.especifications.http.CcpHttpHandler;
@@ -18,15 +17,14 @@ class DbUtilsToElasticSearch implements CcpDbUtils {
 	private CcpMapDecorator connectionDetails = new CcpMapDecorator();
 	
 	public DbUtilsToElasticSearch() {
-		Properties props = new Properties();
+		CcpMapDecorator systemProperties;
 		try {
-			InputStream stream = DbUtilsToElasticSearch.class.getClassLoader().getResource("application1.properties").openStream();
-			props.load(stream);
+			systemProperties = new CcpStringDecorator("application1.properties").propertiesFileFromClassLoader();
 		} catch (Exception e) {
-			
+			systemProperties = new CcpMapDecorator();
 		}
-		Object url = props.getOrDefault("elasticsearch.address", "http://localhost:9200");
-		Object secret = props.getOrDefault("elasticsearch.secret", "");
+		Object url = systemProperties.getOrDefault("elasticsearch.address", "http://localhost:9200");
+		Object secret = systemProperties.getOrDefault("elasticsearch.secret", "");
 		
 		this.connectionDetails = this.connectionDetails.put("Content-Type", "application/json")
 				.put("Content-Type", "application/json").put("DB_URL", url).put("Authorization", secret)
