c497091c19b131fffc596b5ea0108f71b73650a0
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:23 -0300

    .

--- src/main/java/com/ccp/implementations/db/utils/elasticsearch/CcpElasticSearchDbRequest.java
+++ src/main/java/com/ccp/implementations/db/utils/elasticsearch/CcpElasticSearchDbRequest.java
@@ -1,11 +1,11 @@
 package com.ccp.implementations.db.utils.elasticsearch;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.db.utils.CcpDbRequester;
 
-public class CcpElasticSearchDbRequest implements CcpInstanceProvider {
+public class CcpElasticSearchDbRequest implements CcpInstanceProvider<CcpDbRequester> {
 
-	
-	public Object getInstance() {
+	public CcpDbRequester getInstance() {
 		return new ElasticSearchDbRequester();
 	}
 
